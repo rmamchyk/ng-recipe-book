39b2471801b4a17021fc4187908191af5bdc3fe6
rmamchyk <roman.mamchyk@gmail.com>
2018-07-26 13:59:30 +0300

    Add layout for recipe components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Cream Cheese Wontons', 'Cream Cheese Wontons – yet another marriage of American and Chinese cuisines in one addictive, hilariously retro appetizer.', 
+      'https://thewoksoflife.com/wp-content/uploads/2017/12/cream-cheese-wontons-7.jpg'),
+    new Recipe('Sha Cha Beef Stir Fry', 'Sha Cha sauce is most often used in hot pot dipping sauces.',
+      'https://thewoksoflife.com/wp-content/uploads/2018/04/sha-cha-beef-8.jpg')
+  ];
 
   constructor() { }
 
