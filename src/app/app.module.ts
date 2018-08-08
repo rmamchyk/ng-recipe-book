c13a4cc83cb19565fbc42b24ad277350f7d2e738
rmamchyk <roman.mamchyk@gmail.com>
2018-08-08 19:29:40 +0300

    Add home component for displaying a home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,11 +14,12 @@ import { AuthGuard } from './auth/auth-guard.service';
 import { RecipesModule } from './recipes/recipes.module';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [ 
     AppComponent, 
-    HeaderComponent
+    HeaderComponent, HomeComponent
   ],
   imports: [ 
     BrowserModule, 
