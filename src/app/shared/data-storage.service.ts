a4a5751ba4b9e6a5f927707d518db54ad33849b1
rmamchyk <roman.mamchyk@gmail.com>
2018-08-10 11:36:04 +0300

    Replace Http with HttpClient

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,31 +1,24 @@
 import { Injectable } from "@angular/core";
-import { Http, Response } from "@angular/http";
+import { HttpClient } from '@angular/common/http';
+import { throwError } from "rxjs";
+import { map, catchError } from "rxjs/operators";
 
 import { RecipeService } from "../recipes/recipe.service";
-import { map, catchError } from "rxjs/operators";
-import { throwError } from "rxjs";
 import { Recipe } from "../recipes/recipe.model";
 import { AuthService } from "../auth/auth.service";
 
 
 @Injectable()
 export class DataStorageService {
-    constructor(private http: Http,
+    constructor(private httpClient: HttpClient,
         private recipeService: RecipeService,
         private authService: AuthService) {}
 
-    storeRecipes() {
-        const token = this.authService.getToken();
-        return this.http.put('https://ng-recipe-book-2b1d0.firebaseio.com/recipes.json?auth=' + token,
-            this.recipeService.getRecipes());
-    }
-
     fetchRecipes() {
         const token = this.authService.getToken();
-        return this.http.get('https://ng-recipe-book-2b1d0.firebaseio.com/recipes.json?auth=' + token)
+        return this.httpClient.get<Recipe[]>('https://ng-recipe-book-2b1d0.firebaseio.com/recipes.json?auth=' + token)
             .pipe(map(
-                (response: Response) => {
-                    const recipes = response.json();
+                (recipes) => {
                     for(let recipe of recipes) {
                         if (!recipe.ingredients) {
                             recipe.ingredients = [];
@@ -38,10 +31,18 @@ export class DataStorageService {
                 return throwError('Something went wrong');
             }))
             .subscribe(
-                (recipes: Recipe[]) => {
+                (recipes) => {
                     this.recipeService.recipesChanged.next(recipes);
                 },
                 (error) => console.log(error)
             );
     }
+
+    storeRecipes() {
+        const token = this.authService.getToken();
+        return this.httpClient.put('https://ng-recipe-book-2b1d0.firebaseio.com/recipes.json?auth=' + token,
+            this.recipeService.getRecipes());
+    }
+
+
 }
